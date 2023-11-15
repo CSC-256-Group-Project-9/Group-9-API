e9ac58625fcae63a6cef9ef05978ef34ee703fe6
Anthony E <AnthonyDEpps@gmail.com>
2023-11-15 16:43:03 -0500

    create endpoint bug fix

--- router.py
+++ router.py
@@ -22,7 +22,7 @@ def get_db():
 @router.post('/create')
 async def create(request:RequestStudent,db:Session=Depends(get_db)):
     crud.create_student(db,student=request.parameter)
-    return Response(code="200",status="OK",message="Student successfully created").dict(exclude_none=True)
+    return Response(code="200",status="OK",message="Student successfully created",result="").dict(exclude_none=True)
 
 @router.get('/')
 async def get(db:Session=Depends(get_db)):
